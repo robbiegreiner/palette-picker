19cdc2c1f8aa9169e638d298abf6648195843248
Robbie Greiner <robbieg@gmail.com>
2017-11-27 13:49:53 -0700

    serving up static pages

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (request, response) => {
-  response.send('express running ok');
-})
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => {
   console.log(`running on ${app.get('port')}.`);
