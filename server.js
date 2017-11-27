8c009d7a4d7ed390448405074cfe0d43f72cc324
Robbie Greiner <robbieg@gmail.com>
2017-11-27 13:59:38 -0700

    added linter file

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-
 app.use(express.static(__dirname + '/public'));
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`running on ${app.get('port')}.`);
-})
+});
