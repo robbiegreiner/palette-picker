5f2e7685a161a50ed7b6c48b8084ab3657313cf3
Robbie Greiner <robbieg@gmail.com>
2017-11-28 15:40:27 -0700

    setup endpoints

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
+app.get('api/v1/projects', (request, response) => {
+  // JSON projects
+});
+
+app.get('api/v1/palettes', (request, response) => {
+  // JSON palettes
+});
+
+app.post('api/v1/projects', (request, response) => {
+
+});
+
+app.delete('api/v1/palettes:id', (request, response) => {
+  // JSON palettes
+  // request.params.id
+});
+
+app.post('api/v1/palettes', (request, response) => {
+  // JSON palettes
+});
+
+
 
 
 app.listen(app.get('port'), () => {
