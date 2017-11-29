02644933d25f1c8eaf4d453714f16a96177465de
Robbie Greiner <robbieg@gmail.com>
2017-11-29 14:36:05 -0700

    POST to projects working

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
@@ -27,8 +31,22 @@ app.get('/api/v1/palettes', (request, response) => {
     });
 });
 
-app.post('api/v1/projects', (request, response) => {
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
 
+  for (let requiredParameter of ['name']) {
+    if (!project[requiredParameter]) {
+      return response.status(422).send({ error: `You're missing a ${requiredParameter}.` });
+    }
+  }
+
+  database('projects').insert(project, 'id')
+    .then(project => {
+      response.status(201).json({id: project[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
 
 app.delete('api/v1/palettes:id', (request, response) => {
