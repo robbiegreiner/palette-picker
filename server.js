8233900de0933fffcb47d74bc0c1b2c2866a0545
Robbie Greiner <robbieg@gmail.com>
2017-11-29 13:35:14 -0700

    GET projects working

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
-app.get('api/v1/projects', (request, response) => {
-  // JSON projects
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then((projects) => {
+      response.status(200).json(projects);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.get('api/v1/palettes', (request, response) => {
