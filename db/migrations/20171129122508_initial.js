70a1abfde8ba1d97e18514d8483cac1503fe78eb
Robbie Greiner <robbieg@gmail.com>
2017-11-29 12:32:39 -0700

    setup knex file

---
+++ db/migrations/20171129122508_initial.js
@@ -0,0 +1,33 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.createTable('projects', table => {
+      table.increments('id').primary();
+      table.string('name');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('palettes', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('hex1');
+      table.string('hex2');
+      table.string('hex3');
+      table.string('hex4');
+      table.string('hex5');
+      table.integer('project_id').unsigned();
+      table.foreign('project_id').references('projects.id');
+
+      table.timestamps(true, true);
+    })
+
+
+
+  ]); //end of promise all
+};
+
+exports.down = function(knex, Promise) {
+
+};
