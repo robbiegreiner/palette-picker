d54c9052f73311bbbb5257b3f4c5baa3b045862a
Robbie Greiner <robbieg@gmail.com>
2017-11-29 13:06:48 -0700

    wrote seed data code

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -1,13 +1,41 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+
+  return knex('palettes').del()
+    .then(() => knex('projects').del())
+    .then(() => {
+      return Promise.all([
+        knex('projects').insert({
+          id: 1,
+          name: 'idea box'
+        }, 'id')
+        .then(project => {
+          return knex('palettes').insert([
+            {
+              id: 1,
+              name: 'Beastie Boys',
+              hex1: '#A7DF45',
+              hex2: '#81A03C',
+              hex3: '#339445',
+              hex4: '#93B26A',
+              hex5: '#E750C6',
+              project_id: project[0]
+            },
+            {
+              id: 1,
+              name: 'Dirty Heads',
+              hex1: '#769DA3',
+              hex2: '#5CF36D',
+              hex3: '#2236DA',
+              hex4: '#DBA028',
+              hex5: '#BC601A',
+              project_id: project[0]
+            }
+          ]);
+        })
+        .then(() => console.log('The data was successfully seeded!'))
+        .catch(error => console.log(`There was a data seeding error ${ error }`))
+      ])
+      .catch(error => console.log(`There was a data seeding error ${ error }`));
     });
+
 };
