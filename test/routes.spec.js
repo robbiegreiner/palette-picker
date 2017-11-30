4c37bcd1605cd280beea1b46c7439f1e23c5145c
Robbie Greiner <robbieg@gmail.com>
2017-11-30 15:28:18 -0700

    testing individual pallete get

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -123,4 +123,48 @@ describe('API Routes', () => {
     });
   });
 
+  describe('GET /api/v1/projects/:id/palettes', () => {
+    it('should get a project\'s palettes from database', (done) => {
+      chai.request(server)
+      .get('/api/v1/projects/1/palettes')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Beastie Boys');
+        response.body[0].project_id.should.equal(1);
+        response.body[0].should.have.property('hex1');
+        response.body[0].should.have.property('hex2');
+        response.body[0].should.have.property('hex3');
+        response.body[0].should.have.property('hex4');
+        response.body[0].should.have.property('hex5');
+        response.body[0].hex1.should.equal('#A7DF45');
+        response.body[0].hex2.should.equal('#81A03C');
+        response.body[0].hex3.should.equal('#339445');
+        response.body[0].hex4.should.equal('#93B26A');
+        response.body[0].hex5.should.equal('#E750C6');
+
+        response.body[1].should.have.property('name');
+        response.body[1].name.should.equal('Dirty Heads');
+        response.body[1].project_id.should.equal(1);
+        response.body[1].should.have.property('hex1');
+        response.body[1].should.have.property('hex2');
+        response.body[1].should.have.property('hex3');
+        response.body[1].should.have.property('hex4');
+        response.body[1].should.have.property('hex5');
+        response.body[1].hex1.should.equal('#769DA3');
+        response.body[1].hex2.should.equal('#5CF36D');
+        response.body[1].hex3.should.equal('#2236DA');
+        response.body[1].hex4.should.equal('#DBA028');
+        response.body[1].hex5.should.equal('#BC601A');
+        done();
+      })
+      .catch(error => {
+        throw error;
+      });
+    });
+  });
+
 });
