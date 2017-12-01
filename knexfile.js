cc7ca1bdadd4266242b16031534e23f79fedf06a
Robbie Greiner <robbieg@gmail.com>
2017-12-01 10:22:24 -0700

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,15 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + `?ssl=true`,
+  migrations: {
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
+},
+
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/palette_picker_test',
