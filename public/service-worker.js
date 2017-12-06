a4d7f8f7eeb9be5488ae272bf0742a7114121438
Robbie Greiner <robbieg@gmail.com>
2017-12-06 12:32:43 -0700

    added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,38 @@
+this.addEventListener('install', event => {
+  // dont finish installing the service worker until everything in this block is done running
+  event.waitUntil(
+    caches.open('assets-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/js/scripts.js',
+        '/img/padlock-white.svg',
+        '/img/unlock-white.svg',
+        '/css/styles.css'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+this.addEventListener('activate', event => {
+  // caches to keep
+  let cacheWhitelist = ['assets-v1'];
+
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
